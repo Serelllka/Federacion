9209a7dd40383cf4fdbf48622fc7f48898a5d2cd
Serelllka <vprogamervip@gmail.com>
2022-07-15 23:48:22 +0500

    feat: adds handler template

---
+++ pkg/handler/handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"github.com/Serelllka/Federacion/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{
+		services: services,
+	}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up", h.signUp)
+		auth.POST("/sign-in", h.signIn)
+	}
+
+	api := router.Group("/api")
+	{
+		friends := api.Group("/friends")
+		{
+			friends.POST("/add-friend", h.AddFriend)
+		}
+	}
+
+	return router
+}
