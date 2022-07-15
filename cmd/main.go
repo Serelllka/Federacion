d849994cadb11f76efef8fdffd4b020daf86b02e
Serelllka <vprogamervip@gmail.com>
2022-07-16 00:00:17 +0500

    feat: adds basic config

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,25 @@ import (
 	"github.com/Serelllka/Federacion/pkg/handler"
 	"github.com/Serelllka/Federacion/pkg/repository"
 	"github.com/Serelllka/Federacion/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	server := new(federacion.Server)
-	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+func initConfig() error {
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
